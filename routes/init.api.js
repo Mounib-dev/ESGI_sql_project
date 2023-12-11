e4ba40dc2539028ad6b08215354eaa65116800df
M. Mounib OUROUA <ourouamounib@gmail.com>
2023-12-11 11:12:10 +0100

    refactor

--- routes/init.api.js
+++ routes/init.api.js
@@ -4,26 +4,31 @@ const { deleteAllProcedures, addProcedures, executeProcedure, addTriggers, delet
 
 const connection = getConnection();
 
-router.get('/initdb', async(req, res) => {
-    await deleteAllProcedures();
-    await addProcedures();
-
-    await deleteAllTriggers();
-    await addTriggers();
-
-    res.redirect('./call');
-})
-
-router.get('/call', async(req, res) => {
-    executeProcedure('CleanDB')
-    .then(() => executeProcedure('CreateDB'))
-    .then(() => executeProcedure('InsertData'))
-})
-
-
-router.get('/delete-categorie', async (req, res) => {
+router.get('/initdb', (req, res) => {
+    deleteAllProcedures()
+        .then(() => addProcedures())
+        .then(() => deleteAllTriggers())
+        .then(() => addTriggers())
+        .then(() => executeProcedure('CleanDB'))
+        .then(() => executeProcedure('CreateDB'))
+        .then(() => executeProcedure('InsertData'))
+        .then (result => {
+            res.status(200).send("<h1>Success</h1>");
+        })
     
+
 })
+
+// router.get('/call', async(req, res) => {
+//     executeProcedure('CleanDB')
+//     .then(() => executeProcedure('CreateDB'))
+//     .then(() => executeProcedure('InsertData'))
+// })
+
+
+// router.get('/delete-categorie', async (req, res) => {
+    
+// })
 
 
 module.exports = router;
