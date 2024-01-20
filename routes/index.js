060329f60ef7ba2a4727b1fe1d301f15a8ec8c10
VIAUD Nathan <nathan.viaud@inria.fr>
2024-01-20 19:01:25 +0100

    adding crud for morceaux

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const initApi = require("./init.api");
+const morceauApi = require('./morceau.api');
 const artisteApi = require("./artiste.api");
 const categoryApi = require("./category.api");
 
-router.use("/init", initApi);
+router.use('/init', initApi);
 
 router.use("/artiste", artisteApi);
-
 router.use("/category", categoryApi);
+router.use('/morceaux', morceauApi);
 
 module.exports = router;
---
+++ routes/morceau.api.js
@@ -0,0 +1,88 @@
+const router = require('express').Router();
+const { executeQuery, executeProcedureWithParams } = require('../utils/functions');
+
+router.get('', async(req, res) => {
+    try {
+        const result = await executeQuery('SELECT * FROM morceau');
+        res.send(result)
+    } catch(e) {
+        res.status(500).send({ error: e });
+    }
+})
+
+router.get('/:id', async(req, res) => {
+    try {
+
+        const result = await executeQuery(`SELECT * FROM morceau WHERE id = ${req.params.id}`);
+        if(result.length === 0) res.status(404).send({ error: 'Not found' });
+
+        res.send(result);
+    } catch(e) {
+        res.status(500).send({ error: e })
+    }
+})
+
+router.post('/create', async(req, res) => {
+    try {
+        const { titre, duree, date_sortie, id_artiste, id_categorie } = req.body;
+        let query = '';
+        if(!id_categorie) {
+            query = `INSERT INTO morceau (titre, duree, date_sortie, id_artiste) VALUES ('${titre}', ${duree}, '${date_sortie}', ${id_artiste})`;
+        } else {
+            query = `INSERT INTO morceau (titre, duree, date_sortie, id_artiste, id_categorie) VALUES ('${titre}', ${duree}, '${date_sortie}', ${id_artiste}, ${id_categorie})`;
+        }
+        const result = await executeQuery(query);
+
+        res.send(result);
+    } catch(e) {
+        res.status(500).send({ error: e });
+    }
+})
+
+router.post('/addTrackToAlbum/:id', async(req, res) => {
+    try {
+        const { titre, duree, date_sortie, id_category } = req.body;
+        const id_album = req.params.id;
+
+        const result = await executeProcedureWithParams('AddTrackToAlbum', `'${titre}', ${duree}, '${date_sortie}', ${id_album}, ${id_category}`);
+
+        res.send(result);
+    } catch(e) {
+        res.status(500).send({ error: e });
+    }
+})
+
+router.post('/update/:id', async(req, res) => {
+    try {
+        const { titre, duree, date_sortie, id_artiste } = req.body;
+
+        const id_album = req.body.id_album ?? null;
+        const id_categorie = req.body.id_categorie ?? null;
+
+        const id = req.params.id;
+        const query = `UPDATE morceau SET titre = '${titre}', duree = ${duree}, date_sortie = '${date_sortie}', id_artiste = ${id_artiste}, id_categorie = ${id_categorie}, id_album = ${id_album} WHERE id = ${id}`;
+
+        const result = await executeQuery(query);
+
+        res.send(result);
+    } catch(e) {
+        res.status(500).send({ error: e });
+    }
+})
+
+router.delete('/delete/:id', async(req, res) => {
+    try {
+        console.log('delete');
+        const id = req.params.id;
+
+        const query = `DELETE FROM morceau WHERE id = ${id}`;
+
+        const result = await executeQuery(query);
+
+        res.send(result);
+    } catch(e) {
+        res.status(500).send({ error: e })
+    }
+})
+
+module.exports = router;
