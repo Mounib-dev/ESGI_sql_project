8ff5c3ea10231d3be37944b9e33b54334064ea66
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-01-20 16:49:59 +0100

    Adding artiste CRUD routes configuration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
-const router = require('express').Router();
-const initApi = require('./init.api');
+const router = require("express").Router();
+const initApi = require("./init.api");
+const artisteApi = require("./artiste.api");
+const categoryApi = require("./category.api");
 
-router.use('/init', initApi);
+router.use("/init", initApi);
+
+router.use("/artiste", artisteApi);
+
+// router.use("/category", categoryApi);
+
+// router.use('/artiste/create', );
 
 module.exports = router;
