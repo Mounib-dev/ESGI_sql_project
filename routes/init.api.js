79a0066ba2b81816fd4ebc550ef85224c80622dc
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-02-03 16:50:56 +0100

    Refactor init API

--- routes/init.api.js
+++ routes/init.api.js
@@ -1,24 +1,30 @@
-const router = require('express').Router();
-const { getConnection } = require('../utils/config');
-const { deleteAllProcedures, addProcedures, executeProcedure, addTriggers, deleteAllTriggers } = require('../utils/functions');
+const router = require("express").Router();
+const { getConnection } = require("../utils/config");
+const {
+  deleteAllProcedures,
+  addProcedures,
+  executeProcedure,
+  addTriggers,
+  deleteAllTriggers,
+} = require("../utils/functions");
 
 const connection = getConnection();
 
-router.get('/initdb', async (req, res) => {
-    try {
-        await executeProcedure('CleanDB');
-        await executeProcedure('CreateDB');
-        await executeProcedure('InsertData');
-        await deleteAllProcedures();
-        await addProcedures();
-        await deleteAllTriggers();
-        await addTriggers();
+router.get("/initdb", async (req, res) => {
+  try {
+    await executeProcedure("CleanDB");
+    await executeProcedure("CreateDB");
+    await deleteAllProcedures();
+    await addProcedures();
+    await deleteAllTriggers();
+    await addTriggers();
+    await executeProcedure("InsertData");
 
-        res.status(200).send("<h1>Success</h1>");
-    } catch (error) {
-        console.error('Error initializing database:', error);
-        res.status(500).send("<h1>Error initializing database</h1>");
-    }
+    res.status(200).send("<h1>Success</h1>");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    res.status(500).send("<h1>Error initializing database</h1>");
+  }
 });
 
 // router.get('/call', async(req, res) => {
@@ -27,10 +33,8 @@ router.get('/initdb', async (req, res) => {
 //     .then(() => executeProcedure('InsertData'))
 // })
 
-
 // router.get('/delete-categorie', async (req, res) => {
-    
-// })
 
+// })
 
 module.exports = router;
